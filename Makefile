3315a40429a35a0c52c693c68218e36038c0d29b
wakuto <waku.902002@gmail.com>
2021-09-09 14:03:04 +0900

    バイナリの読み込み方法を変更

--- Makefile
+++ Makefile
@@ -3,9 +3,11 @@ OBJS=main.o\
 	pipeline.o\
   cpu.o\
 
+ALL_CH=cpu.h
+HEX=program.hex
 CFLAGS=-Wall -pthread -fsanitize=thread
 
-all: $(OBJS)
+all: $(OBJS) $(ALL_CH)
 	$(CC) $(CFLAGS) $^ -o rv32i_emu
 
 prog_asm: 
@@ -14,13 +16,10 @@ prog_asm:
 	riscv32-unknown-elf-objdump -d program.elf
 
 prog: 
-	riscv32-unknown-elf-gcc -c program.c -o program.o
-	riscv32-unknown-elf-gcc -c start.S -o start.o
+	riscv32-unknown-elf-gcc -mabi=ilp32 -march=rv32i -c program.c -o program.o
+	riscv32-unknown-elf-gcc -mabi=ilp32 -march=rv32i -c start.S -o start.o
 	riscv32-unknown-elf-ld program.o start.o -lc -L/opt/riscv/riscv32-unknown-elf/lib/ -Tlink.ld -static -o program.elf
 	riscv32-unknown-elf-objcopy -O binary program.elf program.bin
-	hexdump -v -e '1/4 "%08x" "\n"' program.bin | sed 's/^/0x/' | sed 's/$$/,/' > program.hex
-	riscv32-unknown-elf-readelf -h program.elf | grep "Entry point address"
-
 
 run:
 	./a.out
--- main.c
+++ main.c
@@ -33,32 +33,43 @@ void setup(void) {
   tcsetattr(STDIN_FILENO, TCSANOW, &term_setting);
 }
 
+void program_writer(char *filename, CPU *cpu) {
+  FILE *program = fopen(filename, "rb");
+  if(program == NULL) {
+    fprintf(stderr, "program.binの取得に失敗しました。\n");
+    exit(1);
+  }
+
+  u32 i = 0;
+  i32 ch;
+  while((ch = fgetc(program)) != EOF) {
+    cpu->mem[i] = 0xFF & ch;
+    i++;
+  }
+  // 命令の長さが4の倍数でない場合は0でパディング
+  if(i % 4 != 0) {
+    for(u32 j = 0; j < 4-(i%4); j++) {
+      cpu->mem[i] = 0x00;
+      i++;
+    }
+  } 
+}
+
 int main(void) {
   setup();
 
   CPU *cpu = (CPU*)calloc(1,sizeof(CPU));
-  int i = 0;
-  int j = 0;
 
-  u32 instruction[] = {
-    #include "program.hex"
-  };
-  u32 inst_count = sizeof(instruction)/sizeof(u32);
   // 命令の書き込み
-  for(j = 0; j < inst_count; j++) {
-    for(i = 0; i < 4; i++) {
-      cpu->mem[i + j*4] = (u8)(instruction[j] >> (i*8));
-    }
-  }
+  program_writer("./program.bin", cpu);
 
   // stackの初期化
   cpu->reg[2] = MEM_SIZE-4;
   // entry pointの設定
-  u32 entry_point = 0x0;
-  cpu->pc = entry_point;
-  mutex_init();
+  cpu->pc = 0;
 
   /*
+  mutex_init();
   pthread_t fetch_thread;
   pthread_t decode_thread;
   pthread_t execute_thread;
@@ -68,7 +79,7 @@ int main(void) {
 
   u32 break_flag = 0;
   u32 break_point = 0xffffffff;
-  i = 0;
+  i32 i = 0;
   for(; cpu->pc < MEM_SIZE; ) {
     if(cpu->pc == break_point) break_flag = 1;
     switch(i) {
